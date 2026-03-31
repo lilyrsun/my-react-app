280fd510efa013797cc6fe78b4b5085fcffa76ec
lilyrsun <l.r.sun@wustl.edu>
2026-03-30 21:07:22 -0500

    create handlePlay function, make Board component fully controlled by props received

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function Square({value, onSquareClick}) {
   );
 }
 
-function Board() {
+function Board({ xIsNext, squares, onPlay }) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -79,6 +79,11 @@ function calculateWinner(squares) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
 
   return (
     <div className="game">
